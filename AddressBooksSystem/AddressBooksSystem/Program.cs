e0b279d596a2aa8bc8b5708383ebc1d7ba96fd58
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-27 23:37:27 +0530

    [kunal]commiting changes for editing contact for address book system program

--- AddressBooksSystem/AddressBooksSystem/Program.cs
+++ AddressBooksSystem/AddressBooksSystem/Program.cs
@@ -4,4 +4,21 @@ using AddressBooksSystem;
 Console.WriteLine("Welcome to Address Book Program");
 AddressBookMain addressMain = new AddressBookMain();
 addressMain.AddContact();
+Console.WriteLine("Select option: \n 1. Add Contact\n 2.Edit Contact\n 3.Exit");
+int num1 = int.Parse(Console.ReadLine());
+
+switch (num1)
+{
+    case 1:
+        addressMain.AddContact();
+        break;
+    case 2:
+        addressMain.EditContact("kunal");
+        break;
+    case 3:
+        break;
+    default:
+        Console.WriteLine("invalid input");
+        break;
+}
 
