337d7546e2c6c1a6ec2d5b9b2ed8f345718021f3
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-27 23:19:22 +0530

    [kunal]commiting changes for adding contact for address book system program

---
+++ AddressBooksSystem/AddressBooksSystem/AddressBookMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBooksSystem
+{
+    public class AddressBookMain
+    {
+        List<Contact> addressBook = new List<Contact>();
+
+        public void AddContact()
+        {
+            Contact contacts = new Contact();
+            Console.WriteLine("Enter FirstName: ");
+            contacts.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName: ");
+            contacts.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Adress: ");
+            contacts.Address = Console.ReadLine();
+            Console.WriteLine("Enter City: ");
+            contacts.City = Console.ReadLine();
+            Console.WriteLine("Enter State: ");
+            contacts.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip: ");
+            contacts.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Phone Number: ");
+            contacts.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter EmailId: ");
+            contacts.Email = Console.ReadLine();
+
+            addressBook.Add(contacts);
+        }
+    }
+}
